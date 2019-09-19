213111d343c04a5625e990a6a00288f820b8462f
Mike <mpetermangis@gmail.com>
2019-09-19 21:39:56 +0900

    toggle section fade

--- ckanext/dfo/fanstatic/dfo.js
+++ ckanext/dfo/fanstatic/dfo.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
         a[0].click();
         a.remove();
     });
-    
+
     $('.section-collapse').click(function() {
         $(this).toggleClass('fa-plus-square fa-minus-square');
+        $(this).closest('.additional-info').find('section-data').toggleFade();
     });
 });
