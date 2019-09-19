f04ffe9b5486cb671155959c40a61fbf5d73bd8e
Mike <mpetermangis@gmail.com>
2019-09-19 21:33:09 +0900

    toggle +/- button

--- ckanext/dfo/fanstatic/dfo.js
+++ ckanext/dfo/fanstatic/dfo.js
@@ -7,4 +7,8 @@ $(document).ready(function() {
         a[0].click();
         a.remove();
     });
+    
+    $('.section-collapse').click(function() {
+        $(this).toggleClass('fa-plus-square fa-minus-square');
+    });
 });
