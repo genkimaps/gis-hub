4c7bb1ce6cab891900ab4edc4f9fa367140d03a4
Mike <mpetermangis@gmail.com>
2019-09-21 15:11:52 +0900

    fill in js content

--- ckanext/dfo/fanstatic/resource_map.js
+++ ckanext/dfo/fanstatic/resource_map.js
@@ -11,24 +11,25 @@ ckan.module('dfo_extent_map', function ($) {
       // Access some options passed to this JavaScript module by the calling
       // template.
       var convexhull = this.options.convexhull;
+      console.log('Current extent: ')
       console.log(convexhull);
+
+      var mymap = L.map('mapid').setView([45.2, -109.6], 8);
+
+      // Tile background layer
+      var esriImagery = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+      var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+      L.tileLayer(
+        esriImagery, {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.esri.com/">ESRI</a>',
+        id: 'mapbox.streets'
+      }).addTo(mymap);
+      
+      L.geoJSON(convexhull).addTo(mymap);
+
     }
   };
 });
-
-
-// var mymap = L.map('mapid').setView([45.2, -109.6], 8);
-
-// // Tile background layer
-// var esriImagery = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-// var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-// L.tileLayer(
-//   esriImagery, {
-//   maxZoom: 18,
-//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//     'Imagery © <a href="https://www.esri.com/">ESRI</a>',
-//   id: 'mapbox.streets'
-// }).addTo(mymap);
-
-// L.geoJSON(convexhull).addTo(mymap);
