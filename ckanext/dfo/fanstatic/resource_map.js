57dbe87bf08726ab406136c51673f038e3ecf238
Mike <mpetermangis@gmail.com>
2019-09-21 15:57:46 +0900

    zoom to layer

--- ckanext/dfo/fanstatic/resource_map.js
+++ ckanext/dfo/fanstatic/resource_map.js
@@ -22,17 +22,19 @@ ckan.module('dfo_extent_map', function ($) {
 
       // Tile background layer
       var esriImagery = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-      var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+      // var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
       L.tileLayer(
         esriImagery, {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.esri.com/">ESRI</a>',
-        id: 'mapbox.streets'
+        // id: 'mapbox.streets'
       }).addTo(mymap);
       
-      L.geoJSON(convexhull).addTo(mymap);
+      var geojsonHull = L.geoJSON(convexhull);
+      geojsonHull.addTo(mymap);
+      mymap.fitBounds(geojsonHull.getBounds());
 
     }
   };
