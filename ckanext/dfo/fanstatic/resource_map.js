50847fbe6691599139ca3416acbd4c4394bc1cda
Mike <mpetermangis@gmail.com>
2019-09-21 15:03:28 +0900

    user theming/javascript.html from ckan docs

--- ckanext/dfo/fanstatic/resource_map.js
+++ ckanext/dfo/fanstatic/resource_map.js
@@ -1,16 +1,34 @@
+// Enable JavaScript's strict mode. Strict mode catches some common
+// programming errors and throws exceptions, prevents some unsafe actions from
+// being taken, and disables some confusing and bad JavaScript features.
+"use strict";
 
-var mymap = L.map('mapid').setView([45.2, -109.6], 8);
+ckan.module('dfo_extent_map', function ($) {
+  return {
+    initialize: function () {
+      console.log("dfo_extent_map initialized for element: ", this.el);
 
-// Tile background layer
-var esriImagery = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-L.tileLayer(
-  esriImagery, {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.esri.com/">ESRI</a>',
-  id: 'mapbox.streets'
-}).addTo(mymap);
+      // Access some options passed to this JavaScript module by the calling
+      // template.
+      var convexhull = this.options.convexhull;
+      console.log(convexhull);
+    }
+  };
+});
 
-L.geoJSON(convexhull).addTo(mymap);
+
+// var mymap = L.map('mapid').setView([45.2, -109.6], 8);
+
+// // Tile background layer
+// var esriImagery = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+// var mapboxImagery = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+// L.tileLayer(
+//   esriImagery, {
+//   maxZoom: 18,
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//     'Imagery © <a href="https://www.esri.com/">ESRI</a>',
+//   id: 'mapbox.streets'
+// }).addTo(mymap);
+
+// L.geoJSON(convexhull).addTo(mymap);
