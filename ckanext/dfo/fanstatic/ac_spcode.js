81b160ec7fc73e3da8b8febf1fc50f0caf218ca7
Mike <mpetermangis@gmail.com>
2020-04-13 17:03:40 +0900

    init template for js sp codes

---
+++ ckanext/dfo/fanstatic/ac_spcode.js
@@ -0,0 +1,11 @@
+var field_name = 'species_codes_js'
+
+
+function load_sp_data(){
+    console.log(sp_data_js)
+}
+
+$(document).ready(function() {
+    console.log('Activate species codes composite field on '+field_name)
+    load_sp_data()
+})
