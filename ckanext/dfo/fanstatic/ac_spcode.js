dcae6b8ce60052a08b85513eddf79d171a949833
Mike <mpetermangis@gmail.com>
2020-04-13 17:22:21 +0900

    tie in table display in form

--- ckanext/dfo/fanstatic/ac_spcode.js
+++ ckanext/dfo/fanstatic/ac_spcode.js
@@ -3,6 +3,18 @@ var field_name = 'species_codes_js'
 
 function load_sp_data(){
     console.log(sp_data_js)
+    var sp_data = JSON.parse(sp_data_str)
+    sp_data.forEach(function(value){
+        var tbl_row = `<tr>
+              <td>${sp_data.sp_code}</td>
+              <td>${sp_data.age_data}</td>
+              <td>${sp_data.obs_type}</td>
+            </tr>`
+        console.log(tbl_row)
+        $('#ac_js_table').append(tbl_row)
+    })
+})
+
 
 }
 
