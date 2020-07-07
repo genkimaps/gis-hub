0c1ad4ae08bb9bcaf56593688ff12dbff600a00e
cole-fields <cole.fields@dfo-mpo.gc.ca>
2020-07-07 10:49:14 -0700

    updated data_maintainer script with env variables

--- ckanext/dfo/dfo_data_maintainer.py
+++ ckanext/dfo/dfo_data_maintainer.py
@@ -17,7 +17,7 @@ def parse_csv(csv_file):
     Return a list of python dictionaries with information about dataset.
     """
     try:
-        with open(csv_file, 'r') as csv:
+        with open(csv_file, "r") as csv:
             dict_reader = DictReader(csv)
             list_of_dicts = list(dict_reader)
             return list_of_dicts
@@ -30,7 +30,7 @@ def read_template(file_name):
     Open email template and return Template object made from its contents.
     """
     try:
-        with open(file_name, 'r', encoding='utf-8') as msg_template:
+        with open(file_name, "r", encoding="utf-8") as msg_template:
             msg_template_content = msg_template.read()
         return Template(msg_template_content)
     except Exception as e:
@@ -39,28 +39,26 @@ def read_template(file_name):
 
 def setup_smtp(dataset_records, message_template):
     # set up the SMTP server
-    s = smtplib.SMTP(host=os.environ.get('CKAN_SMTP_SERVER'), port=587)
-    s.starttls()
-    s.login(os.environ.get('CKAN_SMTP_USER'), os.environ.get('CKAN_SMTP_PASSWORD'))
+    server = smtplib.SMTP(host=os.environ.get("CKAN_SMTP_SERVER"))
+    server.starttls()
+    server.login(os.environ.get("CKAN_SMTP_USER"), os.environ.get("CKAN_SMTP_PASSWORD"))
 
     # For each record (dataset) in csv, send message.
     for record in dataset_records:
         msg = MIMEMultipart()  # create a message
 
         # add in the actual person name to the message template
-        message = message_template.substitute(PERSON_NAME=record['maintainer_name'])
+        message = message_template.substitute(PERSON_NAME=record["maintainer_name"])
 
         # setup the parameters of the message
-        msg['From'] = 'CKAN_SMTP_MAIL_FROM'
-        msg['To'] = record['maintainer_email']
-        msg['Subject'] = "This is TEST"
+        msg["From"] = "CKAN_SMTP_MAIL_FROM"
+        msg["To"] = record["maintainer_email"]
+        msg["Subject"] = "This is TEST"
 
         # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
+        msg.attach(MIMEText(message, "plain"))
 
         # send the message via the server set up earlier.
-        s.send_message(msg)
+        server.send_message(msg)
 
     del msg
-
-parse_csv("F:\\metadata_modified.csv")
