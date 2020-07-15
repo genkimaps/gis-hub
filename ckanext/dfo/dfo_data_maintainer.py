5f23fbd1d872d558a1043b22c246a667dc9d5e0f
cole-fields <cole.fields@dfo-mpo.gc.ca>
2020-07-15 11:32:40 -0700

    try/except and test

--- ckanext/dfo/dfo_data_maintainer.py
+++ ckanext/dfo/dfo_data_maintainer.py
@@ -7,6 +7,7 @@ from csv import DictReader
 import pandas as pd
 from string import Template
 import os
+import sys
 import io
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -19,19 +20,20 @@ def parse_csv(csv_path):
     Return a list of python dictionaries with information about dataset.
     """
     try:
-        with open(csv_path, "r") as csv_file:
-            # Read data from csv as dataframe.
-            data = pd.read_csv(csv_file)
-            # Filter out records with NA values.
-            data = data.dropna()
-            # Filter out records where last modified date was over 365 days ago.
-            data = data[data["days_since_modified"] > 365]
-            # Group data and aggregate unique values from other columns into lists.
-            data_grouped = data.groupby(["title", "maintainer_email", "maintainer_name", "url"],
-                                        as_index=False)["name", "days_since_modified"].agg(lambda x: list(x))
-            # Filter out records with NAs.
-            data_dict = data_grouped.to_dict('r')
-            return data_dict
+        if os.path.exists(csv_path):
+            with open(csv_path, "r") as csv_file:
+                # Read data from csv as dataframe.
+                data = pd.read_csv(csv_file)
+                # Filter out records with NA values.
+                data = data.dropna()
+                # Filter out records where last modified date was over 365 days ago.
+                data = data[data["days_since_modified"] > 50]
+                # Group data and aggregate unique values from other columns into lists.
+                data_grouped = data.groupby(["title", "maintainer_email", "maintainer_name", "url"],
+                                            as_index=False)["name", "days_since_modified"].agg(lambda x: list(x))
+                # Filter out records with NAs.
+                data_dict = data_grouped.to_dict('r')
+                return data_dict
     except Exception as e:
         print(e.args)
 
@@ -56,38 +58,45 @@ def setup_smtp(dataset_records, message_template, subject_template):
     SQL database query. For each record, send an email to the data maintainer with details about the
     dataset they are responsible for.
     """
-    # Set up the SMTP server with mailgun settings.
-    server = smtplib.SMTP(host=os.environ.get("CKAN_SMTP_SERVER"))
+    try:
+        if dataset_records is None:
+            # If dataset records is None, exit program.
+            sys.exit()
+        else:
+            # Set up the SMTP server with mailgun settings.
+            server = smtplib.SMTP(host=os.environ.get("CKAN_SMTP_SERVER"))
 
-    # Start connection to server and login with credentials.
-    server.starttls()
-    server.login(os.environ.get("CKAN_SMTP_USER"), os.environ.get("CKAN_SMTP_PASSWORD"))
+            # Start connection to server and login with credentials.
+            server.starttls()
+            server.login(os.environ.get("CKAN_SMTP_USER"), os.environ.get("CKAN_SMTP_PASSWORD"))
 
-    # For each record (dataset) in csv, send message.
-    for record in dataset_records:
-        msg = MIMEMultipart()
+            # For each record (dataset) in csv, send message.
+            for record in dataset_records:
+                msg = MIMEMultipart()
 
-        # Add in the actual person name to the message template.
-        message = message_template.substitute(PERSON_NAME=record["maintainer_name"],
-                                              DATASET_NAME=record["title"],
-                                              DAYS_SINCE_MODIFIED_MAX=max(record["days_since_modified"]),
-                                              DATA_URL=record["url"])
+                # Add in the actual person name to the message template.
+                message = message_template.substitute(PERSON_NAME=record["maintainer_name"],
+                                                      DATASET_NAME=record["title"],
+                                                      DAYS_SINCE_MODIFIED_MAX=max(record["days_since_modified"]),
+                                                      DATA_URL=record["url"])
 
-        # Add in custom subject with dataset name.
-        subject = subject_template.substitute(DATASET_NAME=record["title"])
+                # Add in custom subject with dataset name.
+                subject = subject_template.substitute(DATASET_NAME=record["title"])
 
-        # Setup the parameters of the message.
-        msg["From"] = os.environ.get("CKAN_SMTP_MAIL_FROM")
-        msg["To"] = record["maintainer_email"]
-        msg["Subject"] = subject
+                # Setup the parameters of the message.
+                msg["From"] = os.environ.get("CKAN_SMTP_MAIL_FROM")
+                msg["To"] = record["maintainer_email"]
+                msg["Subject"] = subject
 
-        # Add the message from template to body of email.
-        msg.attach(MIMEText(message, "plain"))
+                # Add the message from template to body of email.
+                msg.attach(MIMEText(message, "plain"))
 
-        # Send the message via the server set up earlier.
-        server.sendmail(msg["From"], msg["To"], msg.as_string())
+                # Send the message via the server set up earlier.
+                server.sendmail(msg["From"], msg["To"], msg.as_string())
 
-    server.quit()
+            server.quit()
+    except Exception as e:
+        print(e.args, e.message)
     del msg
 
 
