4970825bbc7688ff3764e291ca7effb77ee80337
Fields <FIELDSC@dfo-mpo.ca>
2020-07-17 16:23:11 -0700

    added shell script to execute python code

---
+++ conf/email_maintainer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Activates the virtual environment and executes the python script to email data maintainers referencing the csv file 'maintainer_details.csv' in /tmp
+
+/home/tk/venv/bin/python /home/tk/ckanext-dfo/ckanext/dfo/dfo_data_maintainer.py
