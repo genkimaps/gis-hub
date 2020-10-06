a379860fff8b951992ede7f87f147b6d8723fe17
cole-fields <cole.fields@dfo-mpo.gc.ca>
2020-10-06 12:03:43 -0700

    modified before_view method to include tracking into

--- ckanext/dfo/plugins.py
+++ ckanext/dfo/plugins.py
@@ -209,6 +209,10 @@ class DFOPlugin(p.SingletonPlugin):
             showing in the web browser, but it does not seem to return anything.
             Use after_show() instead, very confusing.
         """
+        pkg_dict = logic.get_action("package_show")({}, {
+            'include_tracking': True,
+            'id': pkg_dict['id']
+        })
         return pkg_dict
 
     # The next two are used by both package and resource
