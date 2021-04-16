0ea8d352097be8e40eb9e2d5af8f3c065fac5ab6
cole-fields <cole.fields@dfo-mpo.gc.ca>
2021-04-16 11:29:51 -0700

    function to get group datasets

--- ckanext/dfo/new_updated_datasets.py
+++ ckanext/dfo/new_updated_datasets.py
@@ -4,8 +4,25 @@ import sys
 import json
 sys.path.append('/home/dfo/hub-geo-api')
 import ckanapi as ck
+import settings
 
-test = ck.get_dataset('seamounts')
+logger = settings.setup_logger()
 
-with open('/tmp/seamounts.json', 'w') as out_file:
-    json.dump(test, out_file)
+
+def process(group_name):
+    datasets_group = ck.package_search(group_name)
+    return datasets_group
+
+
+def main():
+    # CLI arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("group_id", type=str, help="Group name or ID to query and retrieve datasets.")
+    args = parser.parse_args()
+
+    group_datasets = process(args.group_id)
+
+
+if __name__ == "__main__":
+    main()
