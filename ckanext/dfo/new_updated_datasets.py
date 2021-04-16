d733c340ceec5efc48ddd8cda1061db9f482324e
cole-fields <cole.fields@dfo-mpo.gc.ca>
2021-04-16 09:45:59 -0700

    import sys and use path to append py file

--- ckanext/dfo/new_updated_datasets.py
+++ ckanext/dfo/new_updated_datasets.py
@@ -1,7 +1,10 @@
+# Use sys.append since hub-geo-api not set up as a module.
+# https://stackoverflow.com/questions/22955684/how-to-import-py-file-from-another-directory
+import sys
+sys.path.append('/home/dfo/hub-geo-api/ckanapi.py')
 import ckanapi as ck
-import json
 
 test = ck.get_dataset('seamounts')
 
-with open ('/tmp/seamounts.json', 'w') as out_file:
+with open('/tmp/seamounts.json', 'w') as out_file:
     json.dump(test, out_file)
