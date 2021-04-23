c43e08c2de50f26a07a275c8101ca5886c317e6d
cole-fields <cole.fields@dfo-mpo.gc.ca>
2021-04-23 15:29:41 -0700

    troubleshooting

--- ckanext/dfo/new_updated_datasets.py
+++ ckanext/dfo/new_updated_datasets.py
@@ -96,16 +96,21 @@ def send_email(metadata_dict, message_template, subject_template):
             server.quit()
     except Exception as e:
         logger.error("Error loading email settings and sending email...")
+        logger.error(e.args)
 
 
 def latest_modified_date(dataset):
-    # get resources from dataset
-    resources = dataset.get("resources")
-    if resources is not None:
-        # get most recently modified date from list of resources
-        max_date = max(
-            [dateutil.parser.parse(res.get("last_modified")) for res in resources])
-        return max_date
+    try:
+        # get resources from dataset
+        resources = dataset.get("resources")
+        if resources is not None:
+            # get most recently modified date from list of resources
+            max_date = max(
+                [dateutil.parser.parse(res.get("last_modified")) for res in resources])
+            return max_date
+    except Exception as e:
+        logger.error("Error getting last modified date...")
+        logger.error(e.args)
 
 
 def new_or_updated_group(dataset, group_name):
@@ -113,6 +118,7 @@ def new_or_updated_group(dataset, group_name):
     Get all the metadata required to fill template into a dictionary.
     """
     try:
+        logger.info("Processing {}".format(dataset.get("name")))
         date_created = dateutil.parser.parse(dataset.get("metadata_created"))
         # https://docs.ckan.org/en/ckan-2.7.3/maintaining/configuration.html#ckan-display-timezone
         # todays datetime
@@ -186,63 +192,70 @@ def new_or_updated_group(dataset, group_name):
             logger.info("Metadata dictionary prepared for email template...")
             return template_meta
         else:
-            logger.info("Dataset was published {}...".format(date_created))
+            logger.info("{} was published {}...".format(dataset.get("name"), date_created))
     except Exception as e:
         logger.error("Error preparing metadata into a dictionary...")
+        logger.error(e.args)
 
 
 def check_updated(dataset):
-    date_created = dateutil.parser.parse(dataset.get("metadata_created"))
-    # https://docs.ckan.org/en/ckan-2.7.3/maintaining/configuration.html#ckan-display-timezone
-    # todays datetime
-    today = datetime.today()
-    date_diff = today - date_created
-    minutes_diff = date_diff.total_seconds() / 60
-    # get date of most recent modified metadata from dataset
-    res_last_modified_date = latest_modified_date(dataset)
-    res_date_diff = today - res_last_modified_date
-    res_minutes_diff = res_date_diff.total_seconds() / 60
-    logger.info("Checking publication dates of datasets for new records...")
+    try:
+        logger.info("Processing {}".format(dataset.get("name")))
+        date_created = dateutil.parser.parse(dataset.get("metadata_created"))
+        # https://docs.ckan.org/en/ckan-2.7.3/maintaining/configuration.html#ckan-display-timezone
+        # todays datetime
+        today = datetime.today()
+        date_diff = today - date_created
+        minutes_diff = date_diff.total_seconds() / 60
+        # get date of most recent modified metadata from dataset
+        res_last_modified_date = latest_modified_date(dataset)
+        res_date_diff = today - res_last_modified_date
+        res_minutes_diff = res_date_diff.total_seconds() / 60
+        logger.info("Checking publication dates of datasets for updated records...")
 
-    if res_minutes_diff < 60.0 and date_created.date() != today.date():
-        logger.info("Updated dataset found...")
-        # get metadata
-        logger.info("Getting all the metadata from dataset and users to send email...")
-        followers_list = ck.get_dataset_followers(dataset.get("id"))
-        # check if there are any followers
-        if len(followers_list) > 0:
-            # have to get the user IDs in order to filter user list and get emails
-            follower_ids = [follower.get("id") for follower in followers_list]
-            # get list of all users information on gis hub
-            all_users = ck.get_user_info()
-            # now filter the list of all users to get the ones part of the group
-            user_emails = [u.get("email") for u in all_users if u.get("id") in follower_ids]
-            # change history text
-            change_string = dataset.get("change_history")
-            change_list = json.loads(change_string)
-            # get latest change history entry
-            # leave as empty strings so if no change information, the text is blank when the template is filled.
-            change_date = ""
-            change_desc = ""
-            if len(change_list) > 0:
-                change_date = "Change Date: {}".format(change_list[-1].get('change_date'))
-                change_desc = "Description: {}".format(change_list[-1].get('change_description'))
+        if res_minutes_diff < 60.0 and date_created.date() != today.date():
+            logger.info("Updated dataset found...")
+            # get metadata
+            logger.info("Getting all the metadata from dataset and users to send email...")
+            followers_list = ck.get_dataset_followers(dataset.get("id"))
+            # check if there are any followers
+            if len(followers_list) > 0:
+                # have to get the user IDs in order to filter user list and get emails
+                follower_ids = [follower.get("id") for follower in followers_list]
+                # get list of all users information on gis hub
+                all_users = ck.get_user_info()
+                # now filter the list of all users to get the ones part of the group
+                user_emails = [u.get("email") for u in all_users if u.get("id") in follower_ids]
+                # change history text
+                change_string = dataset.get("change_history")
+                change_list = json.loads(change_string)
+                # get latest change history entry
+                # leave as empty strings so if no change information, the text is blank when the template is filled.
+                change_date = ""
+                change_desc = ""
+                if len(change_list) > 0:
+                    change_date = "Change Date: {}".format(change_list[-1].get('change_date'))
+                    change_desc = "Description: {}".format(change_list[-1].get('change_description'))
 
-            # get metadata for email
-            template_meta = {"ds_url": "https://www.gis-hub.ca/dataset/" + dataset.get("name"),
-                             "ds_summary": dataset.get("notes"),
-                             "ds_title": dataset.get("title"),
-                             "emails": user_emails,
-                             "state": "Updated",
-                             "change_date": change_date,
-                             "change_desc": change_desc}
+                # get metadata for email
+                template_meta = {"ds_url": "https://www.gis-hub.ca/dataset/" + dataset.get("name"),
+                                 "ds_summary": dataset.get("notes"),
+                                 "ds_title": dataset.get("title"),
+                                 "emails": user_emails,
+                                 "state": "Updated",
+                                 "change_date": change_date,
+                                 "change_desc": change_desc}
 
-            logger.info("Metadata dictionary prepared for email template...")
-            return template_meta
+                logger.info("Metadata dictionary prepared for email template...")
+                return template_meta
+            else:
+                logger.info("{} has no followers...".format(dataset.get("name")))
         else:
-            logger.info("Dataset has no followers...")
-    else:
-        logger.info("Most recently modified resource date from dataset was: {}".format(res_last_modified_date))
+            logger.info(
+                "Most recently modified resource date from {} was: {}".format(dataset.get("name"), res_last_modified_date))
+    except Exception as e:
+        logger.error("Error preparing metadata into a dictionary...")
+        logger.error(e.args)
 
 
 def process_group(group_name):
