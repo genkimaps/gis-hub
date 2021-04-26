1b9346c94c4a7f5b4de24c2fd8f757d2f6f4da88
cole-fields <cole.fields@dfo-mpo.gc.ca>
2021-04-26 09:52:20 -0700

    tidied up some code

--- ckanext/dfo/new_updated_datasets.py
+++ ckanext/dfo/new_updated_datasets.py
@@ -16,6 +16,53 @@ import json
 sys.path.append("/home/dfo/hub-geo-api")
 import ckanapi as ck
 
+log_dict = {"meta_new": "Preparing metadata from dataset and users to send email...",
+            "meta_updated": "Preparing metadata from dataset resources and users to send email...",
+            "meta_prepared": "Metadata dictionary prepared for email template...",
+            "meta_dict_error": "Error preparing metadata into a dictionary...",
+            "email_template_open": "Opening email template files...",
+            "email_template_fill": "Loading information into email templates...",
+            "email_template_error": "Error opening email templates...",
+            "email_template_arg_error": "Invalid template argument...",
+            "email_params": "Setting up email connection parameters...",
+            "email_send": "Sending email...",
+            "email_send_error": "Error sending email...",
+            "last_modified_error": "Error getting last modified date...",
+            "dataset_new_found": "New dataset found: {}...",
+            "dataset_updated_found": "Updated dataset found: {}...",
+            "no_followers": "{} has no followers...",
+            "all_users_text": "Getting metadata and followers from {} send email..."
+            }
+
+smtp_settings = {"server": os.environ.get("CKAN_SMTP_SERVER"),
+                 "user": os.environ.get("CKAN_SMTP_USER"),
+                 "password": os.environ.get("CKAN_SMTP_PASSWORD"),
+                 "mail_from": os.environ.get("CKAN_SMTP_MAIL_FROM")}
+
+
+class MetaTemplateGroup:
+    def __init__(self, dataset, group_data, group_name, group_user_emails, state, change_date, change_desc):
+        self.ds_url = "https://www.gis-hub.ca/dataset/" + dataset.get("name")
+        self.ds_summary = dataset.get("notes")
+        self.ds_title = dataset.get("title")
+        self.group_title = group_data.get("title")
+        self.group_url = "https://www.gis-hub.ca/group/" + group_name
+        self.emails = group_user_emails
+        self.state = state
+        self.change_date = change_date
+        self.change_desc = change_desc
+
+
+class MetaTemplate:
+    def __init__(self, dataset, user_emails, state, change_date, change_desc):
+        self.ds_url = "https://www.gis-hub.ca/dataset/" + dataset.get("name")
+        self.ds_summary = dataset.get("notes")
+        self.ds_title = dataset.get("title")
+        self.emails = user_emails
+        self.state = state
+        self.change_date = change_date
+        self.change_desc = change_desc
+
 
 # logger setup function
 def logger_setup(output_directory):
@@ -37,14 +84,14 @@ def read_templates(body_file, subject_file):
     Open email message and subject templates and return Template objects made from their contents.
     """
     try:
-        logger.info("Opening email template files...")
+        logger.info(log_dict.get("email_template_open"))
         with io.open(body_file, "r", encoding="utf-8") as msg_template:
             msg_template_content = msg_template.read()
         with io.open(subject_file, "r", encoding="utf-8") as subject_template:
             subject_template_content = subject_template.read()
         return Template(msg_template_content), Template(subject_template_content)
     except Exception as e:
-        logger.error("Error opening email templates...")
+        logger.error(log_dict.get("email_template_error"))
         logger.error(e.args)
 
 
@@ -55,46 +102,46 @@ def send_email(metadata_dict, message_template, subject_template, template="grou
     try:
         if metadata_dict is not None:
             # Set up the SMTP server with mailgun settings.
-            logger.info("Setting up email connection parameters...")
-            server = smtplib.SMTP(host=os.environ.get("CKAN_SMTP_SERVER"))
+            logger.info(log_dict.get("email_params"))
+            server = smtplib.SMTP(host=smtp_settings.get("server"))
 
             # Start connection to server and login with credentials.
             server.starttls()
-            server.login(os.environ.get("CKAN_SMTP_USER"), os.environ.get("CKAN_SMTP_PASSWORD"))
+            server.login(smtp_settings.get("user"), smtp_settings.get("password"))
 
             msg = MIMEMultipart()
 
             # Add in the actual person name to the message template.
-            logger.info("Loading information into email templates...")
+            logger.info(log_dict.get("email_template_fill"))
             if template == "group":
-                message = message_template.substitute(DATASET_NAME=metadata_dict.get("ds_title"),
-                                                      DATA_URL=metadata_dict.get("ds_url"),
-                                                      SUMMARY=metadata_dict.get("ds_summary"),
-                                                      GROUP_NAME=metadata_dict.get("group_title"),
-                                                      GROUP_URL=metadata_dict.get("group_url"),
-                                                      STATE=metadata_dict.get("state"),
-                                                      CHANGE_DATE=metadata_dict.get("change_date"),
-                                                      CHANGE_DESC=metadata_dict.get("change_desc"))
+                message = message_template.substitute(DATASET_NAME=metadata_dict.ds_title,
+                                                      DATA_URL=metadata_dict.ds_url,
+                                                      SUMMARY=metadata_dict.ds_summary,
+                                                      GROUP_NAME=metadata_dict.group_title,
+                                                      GROUP_URL=metadata_dict.group_url,
+                                                      STATE=metadata_dict.state,
+                                                      CHANGE_DATE=metadata_dict.change_date,
+                                                      CHANGE_DESC=metadata_dict.change_desc)
 
                 # Add in custom subject with dataset name.
-                subject = subject_template.substitute(DATASET_NAME=metadata_dict.get("ds_title"),
-                                                      GROUP_NAME=metadata_dict.get("group_title"),
-                                                      STATE=metadata_dict.get("state"))
+                subject = subject_template.substitute(DATASET_NAME=metadata_dict.ds_title,
+                                                      GROUP_NAME=metadata_dict.group_title,
+                                                      STATE=metadata_dict.state)
             elif template == "datasets":
-                message = message_template.substitute(DATASET_NAME=metadata_dict.get("ds_title"),
-                                                      DATA_URL=metadata_dict.get("ds_url"),
-                                                      SUMMARY=metadata_dict.get("ds_summary"),
-                                                      STATE=metadata_dict.get("state"),
-                                                      CHANGE_DATE=metadata_dict.get("change_date"),
-                                                      CHANGE_DESC=metadata_dict.get("change_desc"))
+                message = message_template.substitute(DATASET_NAME=metadata_dict.ds_title,
+                                                      DATA_URL=metadata_dict.ds_url,
+                                                      SUMMARY=metadata_dict.ds_summary,
+                                                      STATE=metadata_dict.state,
+                                                      CHANGE_DATE=metadata_dict.change_date,
+                                                      CHANGE_DESC=metadata_dict.change_desc)
 
                 # Add in custom subject with dataset name.
-                subject = subject_template.substitute(DATASET_NAME=metadata_dict.get("ds_title"),
-                                                      STATE=metadata_dict.get("state"))
+                subject = subject_template.substitute(DATASET_NAME=metadata_dict.ds_title,
+                                                      STATE=metadata_dict.state)
             else:
-                logger.info("Invalid template argument.")
+                logger.info(log_dict.get("email_template_arg_error"))
             # Setup the parameters of the message.
-            msg["From"] = os.environ.get("CKAN_SMTP_MAIL_FROM")
+            msg["From"] = smtp_settings.get("mail_from")
             msg["To"] = ", ".join(metadata_dict.get("emails"))
             msg["Subject"] = subject
 
@@ -102,13 +149,13 @@ def send_email(metadata_dict, message_template, subject_template, template="grou
             msg.attach(MIMEText(message, "plain"))
 
             # Send the message via the server set up earlier.
-            logger.info("Sending email...")
+            logger.info(log_dict.get("email_send"))
             server.sendmail(msg["From"], metadata_dict.get("emails"), msg.as_string())
 
             del msg
             server.quit()
     except Exception as e:
-        logger.error("Error loading email settings and sending email...")
+        logger.error(log_dict.get("email_send_error"))
         logger.error(e.args)
 
 
@@ -123,7 +170,7 @@ def latest_modified_date(dataset):
                  res.get("last_modified") is not None])
             return max_date
     except Exception as e:
-        logger.error("Error getting last modified date...")
+        logger.error(log_dict.get("last_modified_error"))
         logger.error(e.args)
 
 
@@ -132,7 +179,6 @@ def new_or_updated_group(dataset, group_name):
     Get all the metadata required to fill template into a dictionary.
     """
     try:
-        logger.info("Processing {}".format(dataset.get("name")))
         date_created = dateutil.parser.parse(dataset.get("metadata_created"))
         # https://docs.ckan.org/en/ckan-2.7.3/maintaining/configuration.html#ckan-display-timezone
         # todays datetime
@@ -143,12 +189,11 @@ def new_or_updated_group(dataset, group_name):
         res_last_modified_date = latest_modified_date(dataset)
         res_date_diff = today - res_last_modified_date
         res_minutes_diff = res_date_diff.total_seconds() / 60
-        logger.info("Checking publication dates of datasets for new records...")
         # if dataset was created in the last hour, send email to members of org
         if minutes_diff < 60.0:
-            logger.info("New dataset found...")
+            logger.info(log_dict.get("dataset_new_found").format(dataset.get("name")))
             # get group metadata
-            logger.info("Getting all the metadata from dataset and users to send email...")
+            logger.info(log_dict.get("meta_new"))
             group_data = ck.get_group(group_name)
             # have to get the user IDs in order to filter user list and get emails
             group_user_ids = [user.get("id") for user in group_data.get("users")]
@@ -157,23 +202,20 @@ def new_or_updated_group(dataset, group_name):
             # now filter the list of all users to get the ones part of the group
             group_user_emails = [u.get("email") for u in all_users if u.get("id") in group_user_ids]
             # get metadata for email
-            template_meta = {"ds_url": "https://www.gis-hub.ca/dataset/" + dataset.get("name"),
-                             "ds_summary": dataset.get("notes"),
-                             "ds_title": dataset.get("title"),
-                             "group_title": group_data.get("title"),
-                             "group_url": "https://www.gis-hub.ca/group/" + group_name,
-                             "emails": group_user_emails,
-                             "state": "New",
-                             "change_date": "",
-                             "change_desc": ""
-                             }
-            logger.info("Metadata dictionary prepared for email template...")
+            template_meta = MetaTemplateGroup(dataset,
+                                              group_data,
+                                              group_name,
+                                              group_user_emails,
+                                              state="New",
+                                              change_date="",
+                                              change_desc="")
+            logger.info(log_dict.get("meta_prepared"))
             return template_meta
-        # if resources were updated in the last hour, AND dataset was not created today, send email to members of org
+        # if resources were updated in the last hour, AND dataset was not created today, send email to members of group
         elif res_minutes_diff < 60.0 and date_created.date() != today.date():
-            logger.info("Updated dataset found...")
+            logger.info(log_dict.get("dataset_updated_found").format(dataset.get("name")))
             # get group metadata
-            logger.info("Getting all the metadata from dataset and users to send email...")
+            logger.info(log_dict.get("meta_updated"))
             group_data = ck.get_group(group_name)
             # have to get the user IDs in order to filter user list and get emails
             group_user_ids = [user.get("id") for user in group_data.get("users")]
@@ -193,28 +235,23 @@ def new_or_updated_group(dataset, group_name):
                 change_desc = "Description: {}".format(change_list[-1].get('change_description'))
 
             # get metadata for email
-            template_meta = {"ds_url": "https://www.gis-hub.ca/dataset/" + dataset.get("name"),
-                             "ds_summary": dataset.get("notes"),
-                             "ds_title": dataset.get("title"),
-                             "group_title": group_data.get("title"),
-                             "group_url": "https://www.gis-hub.ca/group/" + group_name,
-                             "emails": group_user_emails,
-                             "state": "Updated",
-                             "change_date": change_date,
-                             "change_desc": change_desc}
+            template_meta = MetaTemplateGroup(dataset=dataset,
+                                              group_data=group_data,
+                                              group_name=group_name,
+                                              group_user_emails=group_user_emails,
+                                              state="Updated",
+                                              change_date=change_date,
+                                              change_desc=change_desc)
 
-            logger.info("Metadata dictionary prepared for email template...")
+            logger.info(log_dict.get("meta_prepared"))
             return template_meta
-        else:
-            logger.info("{} was published {}...".format(dataset.get("name"), date_created))
     except Exception as e:
-        logger.error("Error preparing metadata into a dictionary...")
+        logger.error(log_dict.get("meta_dict_error"))
         logger.error(e.args)
 
 
 def check_updated(dataset):
     try:
-        logger.info("Processing {}".format(dataset.get("name")))
         date_created = dateutil.parser.parse(dataset.get("metadata_created"))
         # https://docs.ckan.org/en/ckan-2.7.3/maintaining/configuration.html#ckan-display-timezone
         # todays datetime
@@ -225,12 +262,11 @@ def check_updated(dataset):
         res_last_modified_date = latest_modified_date(dataset)
         res_date_diff = today - res_last_modified_date
         res_minutes_diff = res_date_diff.total_seconds() / 60
-        logger.info("Checking publication dates of datasets for updated records...")
-
+        # if resources have been updated, process and send email
         if res_minutes_diff < 60.0 and date_created.date() != today.date():
-            logger.info("Updated dataset found...")
+            logger.info(log_dict.get("dataset_updated_found").format(dataset.get("name")))
             # get metadata
-            logger.info("Getting all the metadata from dataset and users to send email...")
+            logger.info(log_dict.get("all_users_text").format(dataset))
             followers_list = ck.get_dataset_followers(dataset.get("id"))
             # check if there are any followers
             if len(followers_list) > 0:
@@ -252,24 +288,18 @@ def check_updated(dataset):
                     change_desc = "Description: {}".format(change_list[-1].get('change_description'))
 
                 # get metadata for email
-                template_meta = {"ds_url": "https://www.gis-hub.ca/dataset/" + dataset.get("name"),
-                                 "ds_summary": dataset.get("notes"),
-                                 "ds_title": dataset.get("title"),
-                                 "emails": user_emails,
-                                 "state": "Updated",
-                                 "change_date": change_date,
-                                 "change_desc": change_desc}
+                template_meta = MetaTemplate(dataset=dataset,
+                                             user_emails=user_emails,
+                                             state="Updated",
+                                             change_date=change_date,
+                                             change_desc=change_desc)
 
-                logger.info("Metadata dictionary prepared for email template...")
+                logger.info(log_dict.get("meta_prepared"))
                 return template_meta
             else:
-                logger.info("{} has no followers...".format(dataset.get("name")))
-        else:
-            logger.info(
-                "Most recently modified resource date from {} was: {}".format(dataset.get("name"),
-                                                                              res_last_modified_date))
+                logger.info(log_dict.get("no_followers").format(dataset.get("name")))
     except Exception as e:
-        logger.error("Error preparing metadata into a dictionary...")
+        logger.error(log_dict.get("meta_dict_error"))
         logger.error(e.args)
 
 
