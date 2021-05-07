5130271037194f94f2bbc5a188d6080ab047f46a
cole-fields <cole.fields@dfo-mpo.gc.ca>
2021-05-07 09:12:10 -0700

    modified pg_dump output from sql to .dump as per ckan docs

--- conf/database_backup.sh
+++ conf/database_backup.sh
@@ -6,17 +6,18 @@
 # Use pg_dump utility to backup a PostgreSQL database into a script file in SQL format.
 # Export to /tmp where data can be stored temporarily before being copied to an external storage device.
 #
-# More on pg_dump utility:
+# More on pg_dump utility and CKAN database management:
+# https://docs.ckan.org/en/ckan-2.7.3/maintaining/database-management.html
 # https://postgresql.org/docs/12/app-pg_dump.html
 
 ## Get current date ##
 _now=$(date +"%Y%m%d")
 
 ## Appending a current date from a $_now to a filename stored in $_ckanfile ##
-_ckanfile="/tmp/ckan.$_now.sql"
+_ckanfile="/tmp/ckan.$_now.dump"
 
 ## Appending a current date from a $_now to a filename stored in $_datastorefile ##
-_datastorefile="/tmp/datastore.$_now.sql"
+_datastorefile="/tmp/datastore.$_now.dump"
 
 rm /tmp/**ckan.202**
 rm /tmp/**datastore.202**
