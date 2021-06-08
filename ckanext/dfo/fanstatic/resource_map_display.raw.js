bae5548bf57993560df45ba2e7d88961beeef61f
Mike <mpetermangis@gmail.com>
2021-06-08 18:29:09 +0900

    try extends base

---
+++ ckanext/dfo/fanstatic/resource_map_display.raw.js
@@ -0,0 +1,6 @@
+// This is only used if we are rendering a template raw, without using ckan_extends
+
+// Options passed to this JavaScript module by the calling template.
+// var layer_name = this.options.layername;
+var layer_name = 'vvv'
+console.log('Rendering map preview: '+layer_name)
